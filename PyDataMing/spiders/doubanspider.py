2f4b4ea7b5d358ee60239131326f5ff155ef0b5b
wu-leaf <375790798@qq.com>
2017-04-18 18:38:25 +0800

    finished 10 samples

--- PyDataMing/spiders/doubanspider.py
+++ PyDataMing/spiders/doubanspider.py
@@ -12,9 +12,9 @@ class Douban(CrawlSpider):
 
     name = "douban"
     redis_key = 'douban:start_urls'
-    start_urls = ['http://x.heshuicun.com/forum.php?mod=viewthread&tid=80']
+    start_urls = ['http://bbs.52waha.com/thread-296146-5-1.html']
 
-    url = 'http://x.heshuicun.com/forum.php?mod=viewthread&tid=80'
+    url = 'http://bbs.52waha.com/thread-296146-5-1.html'
 
     def parse(self, response):
         # 源字典
@@ -26,41 +26,123 @@ class Douban(CrawlSpider):
 
 
         selector = Selector(response)
+
+        # 标题筛选
         tit = selector.xpath('//span[@id="thread_subject"]/text()').extract()
-        titl = "".join(tit)
+        if tit:
+            titl = "".join(tit)
+        else:
+            tit = selector.xpath('//*[@id="thread_subject"]/text()').extract()
+        if tit:
+            titl = "".join(tit)
+        else:
+            tit = selector.xpath('//div[@class="bbs-title"]/h2/text()').extract()
+        if tit:
+            titl = "".join(tit)
+        else:
+            tit = selector.xpath('//*[ @ id = "consnav"] / span[4]/text()').extract()
+        if tit:
+            titl = "".join(tit)
+        else:
+            tit = selector.xpath('//*[@class="tit"]/h2/text()').extract()
+        if tit:
+            titl = "".join(tit)
+        else:
+            tit = selector.xpath('//*[@id="subject_tpc"]/text()').extract()
+        if tit:
+            titl = "".join(tit)
 
-        #一次拿到所有时间数据
+
+
+        print '标题',titl
+
+
+
+        # 时间数据筛选
         tim = selector.xpath('//em[starts-with(@id,"authorposton")]/text()').extract()
-        print len(tim)
-        time = "".join(tim[0])
+        if tim:
+            time = "".join(tim[0])
+        else:
+            tim = selector.xpath('// *[ @ class = "bbs-list"] / div[2] / div[1] / span[1]/text()').extract()
+        if tim:
+            time = "".join(tim[0])
+        else:
+            tim = selector.xpath('//*[@xname="date"]/text()').extract()
+        if tim:
+            time = "".join(tim[0])
+        else:
+            tim = selector.xpath('//*[@class="article_foot"]/span/text()').extract()
+        if tim:
+            time = "".join(tim[0])
+        else:
+            tim = selector.xpath('//*[@class="floot_right"]/div[1]/span[2]/text()').extract()
+        if tim:
+            time = "".join(tim[0])
+        else:
+            tim = selector.xpath('//*[@id="my_info_d"]/text()').extract()
+            temp = selector.xpath('//*[@class="my_xiangqing_shijian"]/text()').extract()
+            tim.extend(temp)
+            # 合并列表
+        if tim:
+            time = "".join(tim[0])
 
-        #一次性拿到所有 content
+
+
+        # content数据筛选
         data = selector.xpath('//div[@class="t_fsz"]/table/tr/td[@class="t_f"]')
         con = data.xpath('string(.)').extract()
-        cont = "".join(con[0])
+        if data:
+            cont = "".join(con[0])
+        else:
+            data = selector.xpath('//*[@class="bbs-list"]/div[2]/div[2]')
+            con = data.xpath('string(.)').extract()
+        if data:
+            cont = "".join(con[0])
+        else:
+            #针对第四个 URL ，没抓到 post 内容
+            data = selector.xpath('//*[@class="rconten"]/div[2]')
+            con = data.xpath('string(.)').extract()
+        if data:
+            cont = "".join(con[0])
+        else:
+            data = selector.xpath('//*[@class="text"]')
+            con = data.xpath('string(.)').extract()
+        if data:
+            cont = "".join(con[0])
+        else:
+            data = selector.xpath('//*[@class="tpc_content"]')
+            con = data.xpath('string(.)').extract()
+        if data:
+            cont = "".join(con[0])
+        else:
+            data = selector.xpath('//*[@class= "t_f"]/font')
+            con = data.xpath('string(.)').extract()
+        if data:
+            cont = "".join(con[0])
+
+
 
         # 作者模块搞定
-        post['publish_date'] = time
-        post['content'] = cont
-        post['title'] = titl
+        post['publish_date'] = time.strip()
+        post['content'] = cont.strip()
+        post['title'] = titl.strip()
 
 
         #循环把回复的 字典 添加进入 replys 列表中
         del tim[0]
         del con[0]
 
-        print len(tim)
+        print '回复个数',len(tim)
         i = 0
 
         replys = range(len(tim))
 
 
-
         while i < len(tim):
           replys[i] = {}
-          replys[i]['publish_date'] = "".join(tim[i])
-          replys[i]['content'] = "".join(con[i])
-          replys[i]['title'] = titl
+          replys[i]['publish_date'] = "".join(tim[i]).strip()
+          replys[i]['content'] = "".join(con[i]).strip()
+          replys[i]['title'] = titl.strip()
           i +=1
 
         dict['replys'] = replys
